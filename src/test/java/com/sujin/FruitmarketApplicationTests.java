e96ae8066e6d29fc9bd1f42701e21cbed3fe8125
ifuleave <ifuleav_e@naver.com>
2024-03-27 22:41:47 +0900

    설정

---
+++ src/test/java/com/sujin/FruitmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sujin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
